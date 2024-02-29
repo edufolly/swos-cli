f38ef3186e8791a85cc364b05e4eed829e7f03a3
Eduardo Folly <edufolly@gmail.com>
2024-02-29 19:19:54 -0300

    Updating histogram table.

--- bin/histogram/histogram_list_subcommand.dart
+++ bin/histogram/histogram_list_subcommand.dart
@@ -13,34 +13,28 @@ class HistogramListSubcommand extends StatsListSubcommand {
   ///
   ///
   ///
-  // TODO(edufolly): Create histogram table.
   @override
   List<dynamic> get headers => <String>[
         'Port',
-        'Rx Rate',
-        'Tx Rate',
-        'Rx Package\nRate',
-        'Tx Package\nRate',
-        'Rx Bytes',
-        'Tx Bytes',
-        'Rx Total\nPackets',
-        'Tx Total\nPackets',
+        '64',
+        '65-127',
+        '128-255',
+        '256-511',
+        '512-1023',
+        '1024-max',
       ];
 
   ///
   ///
   ///
-  // TODO(edufolly): Create histogram table.
   @override
   List<dynamic> buildRow(Map<String, dynamic> row) => <dynamic>[
         (row['prt'] as int) + 1,
-        shortNumber(row['rrb']),
-        shortNumber(row['trb']),
-        row['rrp'],
-        row['trp'],
-        shortNumber(row['rb']),
-        shortNumber(row['tb']),
-        shortNumber(row['rtp']),
-        shortNumber(row['ttp']),
+        row['p64'],
+        row['p65'],
+        row['p128'],
+        row['p256'],
+        row['p512'],
+        row['p1k'],
       ];
 }
